01375dae22ba858d512cbb62ae12b60e606c6b3e
Mohd Arshad <mohdarshad8668@gmail.com>
2023-05-31 18:14:05 +0530

    added increment assignment 29/5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import GenerateQuote from './GenerateQuote'
 import Increment from './Increment';
 function App() {
   return (
     <>
-      <GenerateQuote />
-      <br />
       <Increment />
     </>
   );
